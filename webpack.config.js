2847de273a06da541b7489fb6eae43d4dbafc58e
Sujeeban Thuraisamy <s.thuraisamy@qwant.com>
2018-11-13 19:49:54 +0100

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,19 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'masq.js',
     library: 'masq'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
   }
 }
